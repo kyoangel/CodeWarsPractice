2bbe5ca5d65ceceac66b4ca8c5bab4e6d9f16306
kyoangel <gd92x2@hotmail.com>
2017-08-10 23:16:13 +0800

    first test case with customize code snippet

---
+++ FindDeadAnts/FindDeadAnts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindDeadAnts
+{
+    public class FindDeadAnts
+    {
+        public static int DeadAntCount(string ants)
+        {
+            if (string.IsNullOrWhiteSpace(ants))
+                return 0;
+
+            return -1;
+        }
+    }
+}
---
+++ FindDeadAntsTests/FindDeadAntsTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FindDeadAnts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindDeadAnts.Tests
+{
+    [TestClass()]
+    public class FindDeadAntsTests
+    {
+        [TestMethod()]
+        public void Test_DeadAntCount_WHEN_null_THEN_return_zero()
+        {
+            Assert.AreEqual(0, FindDeadAnts.DeadAntCount(null));
+        }
+
+        [TestMethod]
+        public void Test_DeadAntCount_WHEN_ant_THEN_return_zero()
+        {
+            //given
+
+            //when
+
+            //then
+
+        }
+    }
+}
