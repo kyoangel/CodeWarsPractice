8a0503be2f81f6facbd13ee54eec9a8652bff5a6
kyoangel <gd92x2@hotmail.com>
2017-08-20 11:34:19 +0800

    first case input null, get zero

--- CountingDuplicatesTests/CountingDuplicatesTests.cs
+++ CountingDuplicatesTests/CountingDuplicatesTests.cs
@@ -11,21 +11,17 @@ namespace CountingDuplicates.Tests
     [TestClass()]
     public class CountingDuplicatesTests
     {
-        [TestMethod()]
-        public void DuplicateCountTest()
-        {
-            Assert.Fail();
-        }
-
         [TestMethod]
         public void Test_DuplicateCount_WHEN_inputNull_THEN_returnZero()
         {
             //given
+            string input = null;
 
             //when
+            int result = CountingDuplicates.DuplicateCount(input);
 
             //then
-
+            Assert.AreEqual(0, result);
         }
     }
 }
