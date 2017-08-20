8d3bf864ec63deb08298f45cbdba9c0b473a2441
kyoangel <gd92x2@hotmail.com>
2017-08-20 12:44:21 +0800

    [refactor] refactor to linq statement

--- CountingDuplicates/CountingDuplicates.cs
+++ CountingDuplicates/CountingDuplicates.cs
@@ -13,46 +13,7 @@ namespace CountingDuplicates
             if (string.IsNullOrWhiteSpace(str) || str.Length == 1)
                 return 0;
 
-            int length = str.Length;
-            str = str.ToLower();
-
-            Dictionary<char, int> letterCount = PrepareLetterCount(str, length);
-
-            return CalculateDuplicateLetters(letterCount);
-        }
-
-        private static int CalculateDuplicateLetters(Dictionary<char, int> letterCount)
-        {
-            var result = 0;
-
-            foreach (var value in letterCount.Values)
-            {
-                if (value > 0)
-                {
-                    result += 1;
-                }
-            }
-
-            return result;
-        }
-
-        private static Dictionary<char, int> PrepareLetterCount(string str, int length)
-        {
-            Dictionary<char, int> letterCount = new Dictionary<char, int>();
-
-            for (int i = 0; i < length; i++)
-            {
-                if (!letterCount.ContainsKey(str[i]))
-                {
-                    letterCount.Add(str[i], 0);
-                }
-                else
-                {
-                    letterCount[str[i]] += 1;
-                }
-            }
-
-            return letterCount;
+            return str.GroupBy(x => char.ToLower(x)).Where(g => g.Count() > 1).Select(r => r.Key).Count();
         }
     }
 }
