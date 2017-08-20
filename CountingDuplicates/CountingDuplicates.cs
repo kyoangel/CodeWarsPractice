7bdd5d4dc5d07c4466a8e9dabf4cc98b787c5b87
kyoangel <gd92x2@hotmail.com>
2017-08-20 11:30:58 +0800

    add CountingDuplicates

---
+++ CountingDuplicates/CountingDuplicates.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingDuplicates
+{
+    public class CountingDuplicates
+    {
+        public static int DuplicateCount(string str)
+        {
+            if(string.IsNullOrWhiteSpace(str))
+                return 0;
+
+            return -1;
+        }
+    }
+}
---
+++ CountingDuplicatesTests/CountingDuplicatesTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CountingDuplicates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingDuplicates.Tests
+{
+    [TestClass()]
+    public class CountingDuplicatesTests
+    {
+        [TestMethod()]
+        public void DuplicateCountTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Test_DuplicateCount_WHEN_inputNull_THEN_returnZero()
+        {
+            //given
+
+            //when
+
+            //then
+
+        }
+    }
+}
