a9766d35972774151f62a1a7eba72b0f568781ba
kyoangel <gd92x2@hotmail.com>
2017-08-21 20:35:57 +0800

    add sibala project, and first test case

---
+++ Sibala/Dice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibala
+{
+
+    public class Dice
+    {
+        List<int> diceList = new List<int>();
+        public Dice(int p1, int p2, int p3, int p4)
+        {
+            diceList.Add(p1);
+            diceList.Add(p2);
+            diceList.Add(p3);
+            diceList.Add(p4);
+        }
+
+        public string Calculate()
+        {
+
+            return "NoPoint";
+        }
+    }
+}
---
+++ SibalaTests/DiceTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sibala;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibala.Tests
+{
+    [TestClass()]
+    public class DiceTests
+    {
+        [TestMethod]
+        public void Test_Calculate_WHEN_input1234_THEN_returnNoPoint()
+        {
+            //given
+            Dice d = new Dice(1, 2, 3, 4);
+            //when
+            string result = d.Calculate();
+
+            //then
+            Assert.AreEqual("NoPoint", result);
+        }
+    }
+}
