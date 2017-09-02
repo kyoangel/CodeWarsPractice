ca7e12a60bb520d448d51d6d1d3470666458ed70
kyoangel <gd92x2@hotmail.com>
2017-09-02 18:41:15 +0800

    twosum first commit, basic testcase

---
+++ TwoSum/TwoSum.cs
@@ -0,0 +1,20 @@
+﻿namespace TwoSum
+{
+    public class TwoSumLeetCode
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[j] == target - nums[i])
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            throw new System.ArgumentException("No two sum solution");
+        }
+    }
+}
---
+++ TwoSumTests/TwoSumLeetCodeTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TwoSum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum.Tests
+{
+    [TestClass()]
+    public class TwoSumLeetCodeTests
+    {
+        [TestMethod()]
+        public void Test_ZeroArray_Then_Throw_ArgumentException()
+        {
+            var actual = TwoSumLeetCode.TwoSum(new int[] { 3, 2, 3 }, 6);
+            Assert.AreEqual(true, actual.SequenceEqual(new int[] { 0, 2 }));
+        }
+    }
+}
