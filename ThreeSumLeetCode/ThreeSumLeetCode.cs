55ac836cda0e8fb8e67dfa8687da3134bc11ab15
kyoangel <gd92x2@hotmail.com>
2017-09-07 00:39:44 +0800

    while logic, and init testcase

---
+++ ThreeSumLeetCode/ThreeSumLeetCode.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeSumLeetCode
+{
+    public class ThreeSumLeetCode
+    {
+        public static IList<IList<int>> ThreeSum(int[] nums)
+        {
+            int length = nums.Length;
+            IList<IList<int>> result = new List<IList<int>>();
+            Array.Sort(nums);
+
+            int i = 0;
+            int j = 0;
+            int k = 0;
+            while (i < length - 2)
+            {
+                j = i + 1;
+                k = length - 1;
+                while (j < k)
+                {
+                    int threeSum = nums[i] + nums[j] + nums[k];
+                    if (threeSum == 0)
+                    {
+                        result.Add(new List<int> { nums[i], nums[j], nums[k] });
+                    }
+                    if (threeSum >= 0)
+                    {
+                        while (nums[k--] == nums[k] && j < k) ;
+                    }
+                    if (threeSum <= 0)
+                    {
+                        while (nums[j] == nums[++j] && j < k) ;
+                    }
+                }
+                while (nums[i] == nums[++i] && i < length - 2) ;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ThreeSumLeetCodeTests/ThreeSumLeetCodeTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThreeSumLeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeSumLeetCode.Tests
+{
+    [TestClass()]
+    public class ThreeSumLeetCodeTests
+    {
+        [TestMethod()]
+        public void ThreeSumTest()
+        { 
+            var actual = ThreeSumLeetCode.ThreeSum(new int[] { -1, 0, 1, 2, -1, -4 });
+            var expect = new List<IList<int>> { new List<int> { -1, -1, 2 }, new List<int> { -1, 0, 1 } };
+            for (int i = 0; i < expect.Count(); i++)
+            {
+                Assert.IsTrue(actual[i].SequenceEqual(expect[i]));
+            }
+            
+        }
+    }
+}
