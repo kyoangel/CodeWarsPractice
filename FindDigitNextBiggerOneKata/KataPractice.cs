f16c4431f28f9f739b345a72e0e77fc499f512ce
kyoangel <gd92x2@hotmail.com>
2017-09-11 22:01:12 +0800

    init commit, very slow hard code solution...

---
+++ FindDigitNextBiggerOneKata/KataPractice.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace FindDigitNextBiggerOneKata
+{
+    public class KataPractice
+    {
+        public static long NextBiggerNumber(long n)
+        {
+            //code me
+            var targetStr = n.ToString();
+            var length = targetStr.Length;
+            var result = "";
+            var left = 0;
+            var right = 0;
+            for (int i = length - 1; i >= 1; i--)
+            {
+                right = int.Parse(targetStr[i].ToString());
+                left = int.Parse(targetStr[i - 1].ToString());
+                if (left < right)
+                {
+                    var min = targetStr.Substring(i).Where(x=> int.Parse(x.ToString())>left).Min();
+                    var minIndex = targetStr.LastIndexOf(min);
+
+                     result = targetStr.Remove(minIndex, 1);
+
+
+
+                    //result = targetStr.Substring(0, i - 2) + ;
+                    if (i - 2 >= 0)
+                        result = result.Substring(0, i - 1) + min.ToString() + FindMinDigit(result.Substring(i-1));
+                    else
+                        result = min.ToString() + FindMinDigit(result.Substring(i-1));
+                    //for (int j = i; j < length - 1; j++)
+                    //{
+                    //    var result2 = swapChar(result, j + 1, j);
+                    //    if (int.Parse(result2) < int.Parse(result))
+                    //    {
+                    //        return long.Parse(result2);
+                    //    }
+                    //}
+
+                    return long.Parse(result);
+                }
+            }
+            return 0;
+        }
+
+        private static string FindMinDigit(string str)
+        {
+            var result = str.ToCharArray();
+            return new string(result.OrderBy(x => x).ToArray());
+        }
+
+        private static string SwapChar(string str, int v, int i)
+        {
+            var result = str.ToCharArray();
+            var temp = result[v];
+            result[v] = result[i];
+            result[i] = temp;
+
+            return new string(result);
+        }
+    }
+}
---
+++ FindDigitNextBiggerOneKataTests/KataPracticeTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FindDigitNextBiggerOneKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindDigitNextBiggerOneKata.Tests
+{
+    [TestClass()]
+    public class KataPracticeTests
+    {
+        [TestMethod()]
+        public void NextBiggerNumberTest()
+        {
+            Assert.AreEqual(21, KataPractice.NextBiggerNumber(12));
+            Assert.AreEqual(531, KataPractice.NextBiggerNumber(513));
+            Assert.AreEqual(2071, KataPractice.NextBiggerNumber(2017));
+            Assert.AreEqual(441, KataPractice.NextBiggerNumber(414));
+            Assert.AreEqual(414, KataPractice.NextBiggerNumber(144));
+        }
+
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            Assert.AreEqual(1234567908, KataPractice.NextBiggerNumber(1234567890));
+            //Assert.AreEqual(559095479, KataPractice.NextBiggerNumber(559094975));
+            
+        }
+    }
+}
